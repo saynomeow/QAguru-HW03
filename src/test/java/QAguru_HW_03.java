ab4ac7d5a41e41b0ff80feb490115e655c8f2727
saynomeow <aleksey.golikov11@gmail.com>
2021-12-27 19:50:44 +0300

    HW03

---
+++ src/test/java/QAguru_HW_03.java
@@ -0,0 +1,39 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.CollectionCondition.itemWithText;
+import static com.codeborne.selenide.CollectionCondition.texts;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.files.DownloadActions.click;
+
+///Открыть страницу Selenide в Github
+///Перейти в раздел Wiki проекта
+///Убедься, что в списке страниц (Pages) есть страница SoftAssertions
+///Открыть страницу SoftAssertions, проверить что внутри есть пример кода для JUnit5
+
+public class QAguru_HW_03 {
+   //arrange
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        baseUrl = "https://github.com/";
+    }
+
+   //act
+    @Test
+    void testHomeWork03() {
+        open (baseUrl);
+        $("[data-test-selector=nav-search-input]").setValue("Selenide").pressEnter();
+        $$("ul.repo-list li").first().$(".v-align-middle").click();
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+   //assert
+        $("[data-filterable-for=wiki-pages-filter]").shouldHave(text("SoftAssertions")).click();
+        $("[id=wiki-body]").shouldHave(text("Using JUnit5 extend test class:"));
+    }
+}
